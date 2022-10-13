efa59b4de22398ab92126caf14c8c3040bf70334
ZobaerIslam <zobaerislam62@gmail.com>
2022-10-13 20:39:55 +0600

    text to speech and print text

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # virtual-assistant
+
+
+**The fact is, no female voice is included with the core pyttsx3 package. But if you use linux/espeak there is a workaround. You can use one of them to imitate a female voice.**
+- engine.setProperty('voice', 'english+f1')
+- engine.setProperty('voice', 'english+f2')
+- engine.setProperty('voice', 'english+f3')
+- engine.setProperty('voice', 'english+f4')
+- engine.setProperty('voice', 'english_rp+f3') # refer
+- engine.setProperty('voice', 'english_rp+f4')
--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 # Import essential module for this project
 import speech_recognition as sr
+import pyttsx3
 
 # Creating a Recognizer instance
-r = sr.Recognizer()
+
+# Create objects for text to speech
+try:
+    recognizer = sr.Recognizer()
+    ts_eng = pyttsx3.init()
+
+except Exception as e:
+    print(e)
+
+
+# Default female voice and voice rate
+en_female_voice_id = 'english_rp+f3'
+voice_rate = 160
+
+# Set Female voice
+ts_eng.setProperty('voice', en_female_voice_id)   # changes voices. to female
+
+# Set voice rate
+ts_eng.setProperty('rate', voice_rate)    # changes voice rate.
+
+
+# Text to speech
+def talk(text):
+    print(text)
+    ts_eng.say(text)
+    ts_eng.runAndWait()
+
+
+welcome_speech = """Hi, I am Siri, 
+I am your virtual assistant. 
+How may i help you, Sir? """
+
+talk(welcome_speech)
+
 
 try:
     with sr.Microphone() as source:
         print('Listening...')
-        voice = r.listen(source)
-        command = r.recognize_google(voice)
-        print(command)
+        voice = recognizer.listen(source)
+        command = recognizer.recognize_google(voice)
+        command = command.lower()
 
-except:
-    pass
+        talk(command)
+
+except Exception as e:
+    print(e)
